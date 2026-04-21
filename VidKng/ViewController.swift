ddc11de4553bcabc162d9e7171428dba1ba8c2b5
BaziqH <baziq100@gmail.com>
2026-04-21 09:37:27 +0500

    Initial Commit

---
+++ VidKng/ViewController.swift
@@ -0,0 +1,18 @@
+//
+/********** Developed by Drudots Technology **********/
+/******** https://www.drudotstech.com **********/
+//
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
